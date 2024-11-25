1d7a9cb160eb7c9cf1707ff749b484bd2bff6d8c
poteatc <poteatc@outlook.com>
2024-11-25 08:38:11 -0500

    added intro and blank game screen

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        //TODO : print the game intro
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        System.out.println("""
+                     |     |
+                     |     |
+                _____|_____|_____
+                     |     |
+                     |     |
+                _____|_____|_____
+                     |     |
+                     |     |
+                     |     |
+                """);
+    }
+}
