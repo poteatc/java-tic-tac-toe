d5a3292a2b6e75d6b03da4cefcba44a741fd456b
poteatc <poteatc@outlook.com>
2024-11-25 09:30:06 -0500

    Added some logic for the board choices and user input to select one position.. need to re display board with X or O on selected board pos

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,21 +1,40 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean[] board = new boolean[]{
+                false, false, false,
+                false, false, false,
+                false, false, false
+        };
 
         System.out.println("Hello world!");
         //TODO : print the game intro
         System.out.println("Welcome to Tic-Tac-Toe!");
         System.out.println("""
                      |     |
-                     |     |
+                  1  |  2  |  3
                 _____|_____|_____
                      |     |
-                     |     |
+                  4  |  5  |  6
                 _____|_____|_____
                      |     |
-                     |     |
+                  7  |  8  |  9
                      |     |
                 """);
+        System.out.println("Please enter a piece on the board (1 - 9)");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine().trim();
+        int choice = Integer.parseInt(input);
+
+        if (choice <= 0 || choice > 9) {
+            System.out.println("Please enter a valid number...");
+        } else {
+            System.out.println(choice + " selected.");
+            board[choice - 1] = true;
+        }
     }
 }
